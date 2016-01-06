4f1227445c72a334b15a063368898f5bb046e468
Eric Wendelin <me@eriwen.com>
2016-01-05 21:58:01 -0800

    Hopefully fix Travis CI for PRs once and for all.

--- karma.conf.js
+++ karma.conf.js
@@ -14,6 +14,12 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
+        customLaunchers: {
+            Chrome_Travis: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
         browsers: ['PhantomJS2'],
         singleRun: false
     });
